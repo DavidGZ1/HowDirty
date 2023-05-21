0ed083692252e23fb7ecb030b873804dc3443b8e
DavidGZ <49250308+DavidGZ1@users.noreply.github.com>
2023-05-21 14:04:22 +0200

    Renamed NormAbundance to Abundance

--- R/plot_functions.R
+++ R/plot_functions.R
@@ -103,11 +103,11 @@ scale_color_risk <- function(..., option = "RdOrBlu", direction = 1, verbose = F
 #' bmi3(bmi.vals)
 #'
 #' @export
-plot_normabundance <- function(input_conta, scale = "linear"){
+plot_Abundance <- function(input_conta, scale = "linear"){
   # plot the normalized abundance
   # scale: changes the scale to linear or log10; options = c("linear", "log10")
   output <-
-    ggplot(input_conta, aes(x = Analyte, y = NormAbundance)) +
+    ggplot(input_conta, aes(x = Analyte, y = Abundance)) +
     geom_point(aes(color=Risk, text = paste("Replicate: ", ReplicateName, "\nSample: ", Sample)) , alpha = 0.5, size = 1) +
     geom_boxplot( alpha = 0.4, width = 0.2, size = 0.2, outlier.shape = NA, outlier.size = 0, outlier.alpha = 0, outlier.color = NA, outlier.fill = NA) +
     # scale_color_viridis_d(end=0.8, option =  "plasma", direction = 1) +
@@ -145,7 +145,7 @@ plot_pseudochromatogram <- function(input_conta, scale = "linear"){
   # plot the normalized abundance
   # scale: changes the scale to linear or log10; options = c("linear", "log10")
   output <-
-    ggplot(input_conta, aes(x = PeptideRetentionTime, y = NormAbundance, color = Sample)) +
+    ggplot(input_conta, aes(x = PeptideRetentionTime, y = Abundance, color = Sample)) +
     geom_point(aes(text = paste("Replicate: ", ReplicateName, "\nSample: ", Sample,
                                 "\nContaminant: ", Analyte, "\nRisk: ", Risk)), alpha = 0.8, size = 1)  +
     scale_color_viridis_d(end=0.9, option =  "viridis", direction = 1) +
@@ -181,20 +181,20 @@ plot_pseudochromatogram <- function(input_conta, scale = "linear"){
 #' @export
 plot_sample_risk_total <- function(input_conta_summ_sample, order_x = "Sample", scale = "linear"){
   # plot Total Normalized Abundance vs Replicate Name
-  # order = c("Sample", "NormAbundance")
+  # order = c("Sample", "Abundance")
   # scale: changes the scale to linear or log10; options = c("linear", "log10")
-  # arrange in function of Sample or TotalNormAbundance, !!input$order_sample_total didn't work
+  # arrange in function of Sample or TotalAbundance, !!input$order_sample_total didn't work
   if(order_x == "Sample"){
     input_conta_summ_sample <-
       input_conta_summ_sample %>%
       arrange(Sample)
   }else{
-    if(order_x == "NormAbundance"){
+    if(order_x == "Abundance"){
       input_conta_summ_sample <-
         input_conta_summ_sample %>%
-        arrange(desc(TotalNormAbundance))
+        arrange(desc(TotalAbundance))
     }else{
-      stop("order_x must be Sample or NormAbundance")}
+      stop("order_x must be Sample or Abundance")}
   }
 
   input_conta_summ_sample <-
@@ -205,7 +205,7 @@ plot_sample_risk_total <- function(input_conta_summ_sample, order_x = "Sample", 
     # arrange(!! order_x) %>%
     # mutate(Sample = factor(Sample, levels = unique(Sample))) %>%
     mutate(AnalyteGroup = "Total") %>%
-    ggplot(aes(y = TotalNormAbundance, x = Sample, color = RiskLevel, size = TotalNormAbundance)) +
+    ggplot(aes(y = TotalAbundance, x = Sample, color = RiskLevel, size = TotalAbundance)) +
     geom_point(alpha = 1) +
     scale_y_continuous(n.breaks = 10) +
     scale_color_risk() +
@@ -236,54 +236,54 @@ plot_sample_risk_total <- function(input_conta_summ_sample, order_x = "Sample", 
 #'
 #' @export
 plot_sample_risk_analyte <- function(input_conta_summ_sample_risk,
-                                     order_x = "Sample", order_y = "NormAbundance",
+                                     order_x = "Sample", order_y = "Abundance",
                                      show_zeros = FALSE){
-  # plot Anaylte vs Sample, with color in function of risk and size in function of NormAbundance_median
-  # order = c("Sample", "NormAbundance")
+  # plot Anaylte vs Sample, with color in function of risk and size in function of Abundance_median
+  # order = c("Sample", "Abundance")
   # scale: changes the scale to linear or log10; options = c("linear", "log10")
   # remove all zero values
   if(show_zeros == FALSE){
     # remove zero values
     AnalyteGroup2Plot <-
       input_conta_summ_sample_risk %>%
-      group_by(AnalyteGroup, NormAbundance_median) %>%
-      summarise(Keep = all(NormAbundance_median > 0), .groups = "drop") %>%
+      group_by(AnalyteGroup, Abundance_median) %>%
+      summarise(Keep = all(Abundance_median > 0), .groups = "drop") %>%
       filter(Keep == TRUE) %>%
       pull(AnalyteGroup) %>%
       unique()
     input_conta_summ_sample_risk <-
       filter(input_conta_summ_sample_risk, AnalyteGroup %in% AnalyteGroup2Plot)
   }
-  # arrange in function of Sample or NormAbundance, !!input$order_sample_total didn't work
+  # arrange in function of Sample or Abundance, !!input$order_sample_total didn't work
   if(order_x == "Sample"){
     input_conta_summ_sample_risk <-
       input_conta_summ_sample_risk %>%
       arrange(Sample)
   }else{
-    if(order_x == "NormAbundance"){
+    if(order_x == "Abundance"){
       input_conta_summ_sample_risk <-
         input_conta_summ_sample_risk %>%
         group_by(Sample) %>%
-        arrange(desc(median(NormAbundance_median))) %>%
+        arrange(desc(median(Abundance_median))) %>%
         ungroup()
     }else{
-      stop("order_x must be Sample or NormAbundance")}
+      stop("order_x must be Sample or Abundance")}
   }
   input_conta_summ_sample_risk <-
     input_conta_summ_sample_risk %>%
     mutate(Sample = factor(Sample, levels = unique(Sample)))
-  # arrange in function of AnalyteGroup or NormAbundance_median, !!input$order_sample_total didn't work
-  if(order_y == "NormAbundance"){
+  # arrange in function of AnalyteGroup or Abundance_median, !!input$order_sample_total didn't work
+  if(order_y == "Abundance"){
     input_conta_summ_sample_risk <-
       input_conta_summ_sample_risk %>%
-      arrange(NormAbundance_median)
+      arrange(Abundance_median)
   }else{
     if(order_y == "AnalyteGroup"){
       input_conta_summ_sample_risk <-
         input_conta_summ_sample_risk %>%
         arrange(AnalyteGroup)
     }else{
-      stop("order_y must be AnalyteGroup or NormAbundance")}
+      stop("order_y must be AnalyteGroup or Abundance")}
   }
 
   input_conta_summ_sample_risk <-
@@ -293,7 +293,7 @@ plot_sample_risk_analyte <- function(input_conta_summ_sample_risk,
 
   output <-
     input_conta_summ_sample_risk %>%
-    ggplot(aes(y = AnalyteGroup, x = Sample, color = RiskLevel, size = NormAbundance_median)) +
+    ggplot(aes(y = AnalyteGroup, x = Sample, color = RiskLevel, size = Abundance_median)) +
     geom_point() +
     scale_color_risk() +
     ylab("Analyte Group") +
@@ -318,54 +318,54 @@ plot_sample_risk_analyte <- function(input_conta_summ_sample_risk,
 #'
 #' @export
 plot_condition_risk_analyte <- function(input_conta_summ_sample_risk,
-                                        order_x = "Condition", order_y = "NormAbundance",
+                                        order_x = "Condition", order_y = "Abundance",
                                         show_zeros = FALSE){
-  # plot Anaylte vs Condition, with color in function of risk and size in function of NormAbundance_median
-  # order = c("Condition", "NormAbundance")
+  # plot Anaylte vs Condition, with color in function of risk and size in function of Abundance_median
+  # order = c("Condition", "Abundance")
   # scale: changes the scale to linear or log10; options = c("linear", "log10")
   # remove all zero values
   if(show_zeros == FALSE){
     # remove zero values
     AnalyteGroup2Plot <-
       input_conta_summ_sample_risk %>%
-      group_by(AnalyteGroup, NormAbundance_median) %>%
-      summarise(Keep = all(NormAbundance_median > 0), .groups = "drop") %>%
+      group_by(AnalyteGroup, Abundance_median) %>%
+      summarise(Keep = all(Abundance_median > 0), .groups = "drop") %>%
       filter(Keep == TRUE) %>%
       pull(AnalyteGroup) %>%
       unique()
     input_conta_summ_sample_risk <-
       filter(input_conta_summ_sample_risk, AnalyteGroup %in% AnalyteGroup2Plot)
   }
-  # arrange in function of Condition or NormAbundance, !!input$order_sample_total didn't work
+  # arrange in function of Condition or Abundance, !!input$order_sample_total didn't work
   if(order_x == "Condition"){
     input_conta_summ_sample_risk <-
       input_conta_summ_sample_risk %>%
       arrange(Condition)
   }else{
-    if(order_x == "NormAbundance"){
+    if(order_x == "Abundance"){
       input_conta_summ_sample_risk <-
         input_conta_summ_sample_risk %>%
         group_by(Condition) %>%
-        arrange(desc(median(NormAbundance_median))) %>%
+        arrange(desc(median(Abundance_median))) %>%
         ungroup()
     }else{
-      stop("order_x must be Condition or NormAbundance")}
+      stop("order_x must be Condition or Abundance")}
   }
   input_conta_summ_sample_risk <-
     input_conta_summ_sample_risk %>%
     mutate(Condition = factor(Condition, levels = unique(Condition)))
-  # arrange in function of AnalyteGroup or NormAbundance_median, !!input$order_sample_total didn't work
-  if(order_y == "NormAbundance"){
+  # arrange in function of AnalyteGroup or Abundance_median, !!input$order_sample_total didn't work
+  if(order_y == "Abundance"){
     input_conta_summ_sample_risk <-
       input_conta_summ_sample_risk %>%
-      arrange(NormAbundance_median)
+      arrange(Abundance_median)
   }else{
     if(order_y == "AnalyteGroup"){
       input_conta_summ_sample_risk <-
         input_conta_summ_sample_risk %>%
         arrange(AnalyteGroup)
     }else{
-      stop("order_y must be AnalyteGroup or NormAbundance")}
+      stop("order_y must be AnalyteGroup or Abundance")}
   }
 
   input_conta_summ_sample_risk <-
@@ -375,7 +375,7 @@ plot_condition_risk_analyte <- function(input_conta_summ_sample_risk,
 
   output <-
     input_conta_summ_sample_risk %>%
-    ggplot(aes(y = AnalyteGroup, x = Condition, color = RiskLevel, size = NormAbundance_median)) +
+    ggplot(aes(y = AnalyteGroup, x = Condition, color = RiskLevel, size = Abundance_median)) +
     geom_point() +
     scale_color_risk() +
     ylab("Analyte Group") +
