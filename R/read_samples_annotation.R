1ddd6f1857aa8600f0a4b3a171119f9c2d81b29d
DavidGZ <49250308+DavidGZ1@users.noreply.github.com>
2023-05-21 21:17:51 +0200

    read_samples_annotation added autocomplete columns when missing

--- R/read_samples_annotation.R
+++ R/read_samples_annotation.R
@@ -12,9 +12,18 @@
 #'
 #' @export
 read_samples_annotation <- function(file_samples_annotation){
-  read.csv(file_samples_annotation) %>%
+  output <-   read.csv(file_samples_annotation)
+  # fill columns if not included
+  if(is.null(output$Sample)) output$Sample = output$ReplicateName
+  if(is.null(output$Condition)) output$Condition = "All Samples"
+  if(is.null(output$DilutionFactor)) output$DilutionFactor = NA
+
+  output <-
+    output %>%
     rename(ReplicateName = ReplicateNameSkyline) %>%
     mutate(across(where(is.character), ~factor(.x, levels = unique(.x)))) %>% # create levels in given order
     mutate(DilutionFactor = replace_na(DilutionFactor, 1))  # complete dilution factor if empty
+
+  return(output)
 
 }
