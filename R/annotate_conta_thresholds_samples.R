05d73699692a70fe20caa474bd849f559545df01
DavidGZ <49250308+DavidGZ1@users.noreply.github.com>
2023-06-19 23:48:14 +0200

    created annotate_conta_thresholds_samples

---
+++ R/annotate_conta_thresholds_samples.R
@@ -0,0 +1,27 @@
+#' Short description
+#'
+#' description
+#'
+#' @param x type of input object (e.g. numeric vector).
+#'
+#' @return type of output object (e.g. numeric vector).
+#'
+#' @examples
+#' bmi.vals <- rnorm(n = 50, mean = 25, sd = 3)
+#' bmi3(bmi.vals)
+#'
+#' @export
+annotate_conta_thresholds_samples <- function(df_conta_summ_sample, df_threshold_sample){
+  require(mgsub)
+  df_conta_summ_sample %>%
+    mutate(Risk =
+             cut(x = Abundance_total,
+                 breaks = df_threshold_sample$Break,
+                 labels = df_threshold_sample$Labels[-1]))  %>%
+    drop_na() %>%
+    # select(Condition, Sample, ReplicateName, TotalAbundance = Abundance_total, Risk) %>%
+    # arrange(Condition, Sample, ReplicateName)
+    mutate(RiskLevel= as.numeric(substr(as.character(Risk),1,1)),
+           RiskLevel = factor(RiskLevel, levels = c(0:6))) %>%
+    relocate(Condition, Sample, ReplicateName, Abundance_total, RiskLevel, Risk)
+}
